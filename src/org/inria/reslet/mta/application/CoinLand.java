1a04628c63c9b1be4ff95a3399d407c40d3f97c9
Romiche <Romiche@LAPTOP-J3QHH7K5>
2018-12-07 13:24:58 +0100

    Packages

--- src/org/inria/reslet/mta/application/CoinLand.java
+++ src/org/inria/reslet/mta/application/CoinLand.java
@@ -1,5 +1,67 @@
 package org.inria.reslet.mta.application;
 
+import java.util.ArrayList;
+import org.inria.reslet.mta.internals.*;
+
 public class CoinLand {
+	
+	private ArrayList<Attraction> attractions;
+	private ArrayList<Clients> clients;
+	private ArrayList<Navette> navettes1;
+	private ArrayList<Navette> navettes2;
+	private ResponsableBilleterie rb;
+	private Billeterie billeterie;
+
+	public CoinLand() {
+		init();
+	}
+
+	public void init() {
+		this.billeterie = new Billeterie(20);
+		this.clients = new ArrayList<Clients>();
+		this.navettes1 = new ArrayList<Navette>();
+		this.navettes2 = new ArrayList<Navette>();
+
+		Attraction VieuxChienFantôme = new Attraction(1, 400, 200);
+		Navette n1 = new Navette(1, 10, VieuxChienFantôme);
+		Navette n2 = new Navette(2, 10, VieuxChienFantôme);
+		navettes1.add(n1);
+		navettes1.add(n2);
+		VieuxChienFantôme.setListeNavettes(navettes1);
+
+		Attraction CoinCoin = new Attraction(2, 400, 200);
+		Navette n3 = new Navette(15, 3, CoinCoin);
+		Navette n4 = new Navette(15, 4, CoinCoin);
+		navettes2.add(n3);
+		navettes2.add(n4);
+		CoinCoin.setListeNavettes(navettes2);
+
+		attractions.add(VieuxChienFantôme);
+		attractions.add(CoinCoin);
+		this.rb = new ResponsableBilleterie(1, this.billeterie);
+		for (int i = 1; i < 2; i++) {
+			clients.add(new Clients(1, this.attractions, i, this.billeterie));
+		}
+
+	}
+
+	public void depart() {
+		for (Navette n : navettes1) {
+			n.start();
+		}
+		for (Navette n : navettes2) {
+			n.start();
+		}
+		for (Clients c : clients) {
+			c.start();
+		}
+		rb.start();
+	}
+
+	public static void main(String[] args) {
+		CoinLand p = new CoinLand();
+		p.depart();
+
+	}
 
 }
--- src/org/inria/reslet/mta/internals/Navette.java
+++ src/org/inria/reslet/mta/internals/Navette.java
@@ -6,65 +6,64 @@ public class Navette extends Thread {
 	public int personnes_dans_navette;
 	public int id_navette;
 	public boolean fin_du_tour;
-	
-	public Navette(int places_navette,Attraction attraction,int id_navette) {
+
+	public Navette(int id_navette, int places_navette, Attraction attraction) {
+		this.id_navette = id_navette;
 		this.places_navette = places_navette;
 		this.attraction = attraction;
 		this.personnes_dans_navette = 0;
-		this.id_navette = id_navette;
 		this.fin_du_tour = false;
 		this.setDaemon(true);
 	}
-	
-	public synchronized Navette monterDansNavette(int id_client) {
-		if(this.personnes_dans_navette == this.places_navette) {
+
+	public synchronized Navette monterDansNavette(int id_client) throws InterruptedException {
+		while (this.personnes_dans_navette == this.places_navette) {
 			System.out.println("Navette pleine! car " + this.personnes_dans_navette + " = " + this.places_navette);
-			return null;
-		}
-		else {
-			System.out.println("Le client " + id_client + " monte dans la navette " + id_navette +
-					" dans l'attraction " + attraction.getIDAttraction());
-			this.personnes_dans_navette ++;
-			System.out.println("Il y a " + this.personnes_dans_navette + " personnes dans la navette " + this.id_navette + " "+
-			"pour un total de " + this.places_navette + " places dans la navette");
-		}
-		return this;
-	}
-	
-	public synchronized void descendreDeNavette(int id_client) throws InterruptedException {
-		while(!fin_du_tour) {
 			wait();
 		}
-		System.out.println("Le client " + id_client + " descend de la navette " + this.id_navette +
-				" dans l'attraction " + attraction.getIDAttraction());
+		System.out.println("Le client " + id_client + " monte dans la navette " + id_navette + " dans l'attraction "
+				+ attraction.getIDAttraction());
+		this.personnes_dans_navette++;
+		System.out.println("Il y a " + this.personnes_dans_navette + " personnes dans la navette " + this.id_navette
+				+ " " + "pour un total de " + this.places_navette + " places dans la navette");
+
+		return this;
+	}
+
+	public synchronized void descendreDeNavette(int id_client) throws InterruptedException {
+		while (!fin_du_tour) {
+			wait();
+		}
+		System.out.println("Le client " + id_client + " descend de la navette " + this.id_navette
+				+ " dans l'attraction " + attraction.getIDAttraction());
 		this.personnes_dans_navette--;
 		notifyAll();
 	}
-	
+
 	public int getPlacesNavette() {
 		return this.places_navette;
 	}
-	
+
 	public int getPersonnesDansNavette() {
 		return this.personnes_dans_navette;
 	}
-	
+
 	public int getIDNavette() {
 		return this.id_navette;
 	}
-	
+
 	public synchronized void setFinDuTour(boolean fin) {
 		fin_du_tour = fin;
-		if(fin_du_tour) {
+		if (fin_du_tour) {
 			System.out.println("La navette " + this.id_navette + " a fini son tour");
 			notifyAll();
 		}
 	}
-	
+
 	public void run() {
 		try {
 			attraction.arriveAQuai(id_navette);
-			while(true) {
+			while (true) {
 				sleep(attraction.temps_a_quai);
 				attraction.parsDuQuai(id_navette);
 				sleep(attraction.temps_attraction);
